14e1d1a993a78214b52d3ed7ad07fe89b8e14829
Destiny <desto4q@gmail.com>
2024-02-17 02:01:24 +0100

    remove tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 function Home() {
-	return <div>Home</div>;
+	const [opened, { toggle }] = useDisclosure();
+	return (
+		<AppShell
+			header={{ height: 60 }}
+			navbar={{
+				width: 300,
+				breakpoint: "sm",
+				collapsed: { mobile: !opened, desktop: !opened },
+			}}
+			padding="md"
+		>
+			<AppShell.Header >
+				<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+				<div>Logo</div>
+			</AppShell.Header>
+
+			<AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+			<AppShell.Main>Main</AppShell.Main>
+		</AppShell>
+	);
 }
 
 export default Home;
